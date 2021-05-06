de135cdf6381c37e4bb1cea53f2a505a4ba31295
LaloDavilaC9 <eduardo.davilac9@gmail.com>
2021-05-06 12:12:05 -0500

    Create metodosConsultas

---
+++ EjemploAntesProyecto/src/consultas/metodosConsultas.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consultas;
+
+public class metodosConsultas {
+    public metodosConsultas(){
+        
+    }
+    public void imprimirHola(){
+        System.out.println("Hola este es un metodo creado por Lalo");
+    }
+    
+}
