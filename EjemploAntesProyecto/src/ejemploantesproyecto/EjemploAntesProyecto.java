87c5c138e1e389e04afc9c38d3c165d214f8672c
VictorLuna-ISC <victor_delgado1369@hotmail.com>
2021-05-06 12:07:23 -0500

    Agrege hola victor
    Agregue el sout de hola victor.
    
    Co-Authored-By: LuisBA31 <74994999+LuisBA31@users.noreply.github.com>
    Co-Authored-By: LaloDavilaC9 <36578173+LaloDavilaC9@users.noreply.github.com>

--- EjemploAntesProyecto/src/ejemploantesproyecto/EjemploAntesProyecto.java
+++ EjemploAntesProyecto/src/ejemploantesproyecto/EjemploAntesProyecto.java
@@ -4,6 +4,7 @@ package ejemploantesproyecto;
 public class EjemploAntesProyecto {
     public static void main(String[] args) {
         System.out.println("holi");
+        System.out.println("Hola Victor");
     }
  
 }
